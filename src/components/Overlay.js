9b869a49df466bb6712605d208a6db045716f5d0
beez0mbie <vzhik3268@gmail.com>
2022-12-03 19:56:57 +0300

    fix: overflow autho

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -16,78 +16,83 @@ const Overlay = ({ onCloseCart, items = [], onRemove }) => {
             onClick={onCloseCart}
           />
         </div>
-        {/* <div className='cartItems'>
-          {items.map((item) => (
-            <div
-              key={item.title}
-              className='cartItem'
-            >
-              <img
-                className='cartImg'
-                src={item.imgUrl}
-                alt='Sneakers'
-              />
-              <div className='cartItemTextContent'>
-                <p className='cartItemName'>{item.title}</p>
-                <b className='cartItemPrice'>{item.price} руб.</b>
-              </div>
-              <img
-                className='removeBtn'
-                src='/img/btn-remove.svg'
-                alt='Remove'
-                onClick={() => handleRemoveClick(item)}
-              />
+        {items.length > 0 ? (
+          <div className='cartContent'>
+            <div className='cartItems'>
+              {items.map((item) => (
+                <div
+                  key={item.title}
+                  className='cartItem'
+                >
+                  <img
+                    className='cartImg'
+                    src={item.imgUrl}
+                    alt='Sneakers'
+                  />
+                  <div className='cartItemTextContent'>
+                    <p className='cartItemName'>{item.title}</p>
+                    <b className='cartItemPrice'>{item.price} руб.</b>
+                  </div>
+                  <img
+                    className='removeBtn'
+                    src='/img/btn-remove.svg'
+                    alt='Remove'
+                    onClick={() => handleRemoveClick(item)}
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
 
-        <div className='cartTotalBlock'>
-          <ul>
-            <li className='cartPrice'>
-              <span>Итого:</span>
-              <div className='dashed'></div>
-              <b>21 498 руб.</b>
-            </li>
-            <li className='cartPrice'>
-              <span>Налог 5%</span>
-              <div className='dashed'></div>
-              <b>1074 руб.</b>
-            </li>
-          </ul>
+            <div className='cartTotalBlock'>
+              <ul>
+                <li className='cartPrice'>
+                  <span>Итого:</span>
+                  <div className='dashed'></div>
+                  <b>21 498 руб.</b>
+                </li>
+                <li className='cartPrice'>
+                  <span>Налог 5%</span>
+                  <div className='dashed'></div>
+                  <b>1074 руб.</b>
+                </li>
+              </ul>
 
-          <button className='greenButton'>
-            Оформить заказ
-            <img
-              src='/img/arrow.svg'
-              alt='Arrow'
-              className='arrowImg'
-            />
-          </button>
-        </div> */}
-        <div className='emptyCart'>
-          <img
-            className='emptyCartImage'
-            src='/img/empty-cart.jpg'
-            alt='EmptyCart'
-          />
-          <div>
-            <h3 className='eptyCartTitle'>Корзина пустая</h3>
-            <p className='eptyCartText'>
-              Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.
-            </p>
+              <button className='greenButton'>
+                Оформить заказ
+                <img
+                  src='/img/arrow.svg'
+                  alt='Arrow'
+                  className='arrowImg'
+                />
+              </button>
+            </div>
           </div>
-          <button
-            onClick={onCloseCart}
-            className='greenButton'
-          >
-            Вернуться назад
+        ) : (
+          <div className='emptyCart'>
             <img
-              src='/img/arrow-left.svg'
-              alt='Arrow'
-              className='arrowImg'
+              className='emptyCartImage'
+              src='/img/empty-cart.jpg'
+              alt='EmptyCart'
             />
-          </button>
-        </div>
+            <div>
+              <h3 className='eptyCartTitle'>Корзина пустая</h3>
+              <p className='eptyCartText'>
+                Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.
+              </p>
+            </div>
+            <button
+              onClick={onCloseCart}
+              className='greenButton'
+            >
+              Вернуться назад
+              <img
+                src='/img/arrow-left.svg'
+                alt='Arrow'
+                className='arrowImg'
+              />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
